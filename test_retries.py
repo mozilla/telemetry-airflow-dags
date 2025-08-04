6a04ef11c42b16e39975ad2475528a2f7b7d1657
Sean Rose <srose@mozilla.com>
2025-08-04 14:35:34 -0700

    Add `test_retries` DAG.

---
+++ test_retries.py
@@ -0,0 +1,23 @@
+from datetime import datetime, timedelta, timezone
+
+from airflow import DAG
+from airflow.decorators import task
+
+default_args = {
+    "email_on_retry": False,
+    "email_on_failure": False,
+    "retries": 2,
+    "retry_delay": timedelta(seconds=15),
+}
+
+with DAG(
+    "test_retries",
+    default_args=default_args,
+    schedule_interval="* * * * *",
+    start_date=datetime(2025, 8, 4, 21, 30, tzinfo=timezone.utc),
+    catchup=False,
+) as dag:
+    @task
+    def failing_task():
+        raise Exception('You shall not pass!')
+    failing_task()
